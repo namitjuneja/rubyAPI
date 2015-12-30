441f3ae6164c798622f67dde9f7db338139d85df
Your Name <juneja.namit@gmail.com>
2015-12-30 17:05:33 +0530

    add allow access response headers to type2/index

--- app/controllers/type2_controller.rb
+++ app/controllers/type2_controller.rb
@@ -3,6 +3,7 @@ class Type2Controller < ApplicationController
   	@user = {"name" => 'Max', "email" => ' m@m.com'}
   	puts params[:foo]
   	@data = {"content" => Type2.find(params[:num])["content"]}
+    response.headers['Access-Control-Allow-Origin'] = '*'
   	render json: @data
   end
   def index2
