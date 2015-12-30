616f77e564c86926c31394cd50095641e15de448
Your Name <juneja.namit@gmail.com>
2015-12-30 15:38:46 +0530

    db add

--- app/controllers/type2_controller.rb
+++ app/controllers/type2_controller.rb
@@ -6,7 +6,13 @@ class Type2Controller < ApplicationController
   	render json: @data
   end
   def index2
-  	Type1.create(:head=> "21", :content => "21")
+
+  	Type1.create(:head=> "2", :content => "Data 2")
+  	Type1.create(:head=> "3", :content => "Data 3")
+  	Type1.create(:head=> "4", :content => "Data 4")
+  	Type1.create(:head=> "5", :content => "Data 5")
+
+
   	@user = {"name" => 'Max', "email" => ' m@m.com'}
   	render json: @user
   end
